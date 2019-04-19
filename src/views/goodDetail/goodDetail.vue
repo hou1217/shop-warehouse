8ce1048fd5fe6825f7e1f3dbccb57a519207fd22
yzy <yezeyu@wallan-tech.com>
2019-04-19 20:23:19 +0800

    2019/4/19 20:22 1.修改样式

--- src/views/goodDetail/goodDetail.vue
+++ src/views/goodDetail/goodDetail.vue
@@ -1,217 +1,218 @@
 <template>
   <div class="container container_good-detail">
-    <!-- todo 图片轮播 -->
-    <div class="good-images-box"></div>
+      <!-- todo 图片轮播 -->
+      <div class="good-images-box"></div>
+  
+      <!-- 主体盒子 -->
+      <div class="main-box main-box_good-detail">
+        <!-- 商品介绍 -->
+        <div class="good-desc">
+          <div class="top">
+            <div class="name">
+              商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商称商品名称商品名...
+            </div>
+            <div class="money-box money-box_price">
+              <div class="money-box__left">
+                <div class="character">售价</div>
+                <div class="num">
+                  <div class="icon">
+                    <img src="@/assets/images/shop_money@2x.png"/>
+                  </div>
+                  <div class="text">39.6</div>
+                </div>
+              </div>
+              <div class="money-box__right">
+                <div class="character">今日进货</div>
+                <div class="num">3323</div>
+              </div>
+            </div>
+            <div class="money-box money-box_profit">
+              <div class="money-box__left">
+                <div class="character">毛利</div>
+                <div class="num">
+                  <div class="icon">
+                    <img src="@/assets/images/shop_money@2x.png"/>
+                  </div>
+                  <div class="text">8.5</div>
+                </div>
+              </div>
+              <div class="money-box__right">
+                <div class="character">总仓库存</div>
+                <div class="num">5432</div>
+              </div>
+            </div>
+          </div>
+          <div class="middle">
+            <div class="activity">
+              <div class="character">活动</div>
+              <div class="text">优惠活动信息</div>
+              <div class="next-btn">
+                <img src="@/assets/images/arrow_enter_g@2x.png">
+              </div>
+            </div>
+            <div class="service">
+              <div class="character">服务</div>
+              <div class="text">服务内容</div>
+              <div class="next-btn">
+                <img src="@/assets/images/arrow_enter_g@2x.png">
+              </div>
+            </div>
+            <div class="dispatch">
+              <div class="character">配送</div>
+              <div class="text">今天21:09</div>
+              <div class="next-btn">
+                <img src="@/assets/images/arrow_enter_g@2x.png">
+              </div>
+            </div>
+          </div>
+          <div class="bottom">
+            <div class="character">已选</div>
+            <div class="text">商品规格</div>
+            <div class="next-btn">
+              <img src="@/assets/images/arrow_enter_g@2x.png">
+            </div>
+          </div>
+        </div>
     
-    <!-- 主体盒子 -->
-    <div class="main-box main-box_good-detail">
-      <!-- 商品介绍 -->
-      <div class="good-desc">
-        <div class="top">
-          <div class="name">
-            商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商称商品名称商品名...
-          </div>
-          <div class="money-box money-box_price">
-            <div class="money-box__left">
-              <div class="character">售价</div>
-              <div class="num">
-                <div class="icon">
-                  <img src="@/assets/images/shop_money@2x.png"/>
-                </div>
-                <div class="text">39.6</div>
-              </div>
+        <!-- 商品数据 -->
+        <div class="good good_report">
+          <div class="title">
+            <div class="left-block"></div>
+            <div class="text">商品数据</div>
+            <div class="report-icon">
+              <img src="@/assets/images/shop_report@2x.png">
             </div>
-            <div class="money-box__right">
-              <div class="character">今日进货</div>
-              <div class="num">3323</div>
-            </div>
-          </div>
-          <div class="money-box money-box_profit">
-            <div class="money-box__left">
-              <div class="character">毛利</div>
-              <div class="num">
-                <div class="icon">
-                  <img src="@/assets/images/shop_money@2x.png"/>
-                </div>
-                <div class="text">8.5</div>
-              </div>
-            </div>
-            <div class="money-box__right">
-              <div class="character">总仓库存</div>
-              <div class="num">5432</div>
-            </div>
-          </div>
-        </div>
-        <div class="middle">
-          <div class="activity">
-            <div class="character">活动</div>
-            <div class="text">优惠活动信息</div>
             <div class="next-btn">
-              <img src="@/assets/images/arrow_enter_g@2x.png">
+              <img src="@/assets/images/arrow_enter_b@2x.png">
             </div>
           </div>
-          <div class="service">
-            <div class="character">服务</div>
-            <div class="text">服务内容</div>
-            <div class="next-btn">
-              <img src="@/assets/images/arrow_enter_g@2x.png">
-            </div>
-          </div>
-          <div class="dispatch">
-            <div class="character">配送</div>
-            <div class="text">今天21:09</div>
-            <div class="next-btn">
-              <img src="@/assets/images/arrow_enter_g@2x.png">
-            </div>
-          </div>
-        </div>
-        <div class="bottom">
-          <div class="character">已选</div>
-          <div class="text">商品规格</div>
-          <div class="next-btn">
-            <img src="@/assets/images/arrow_enter_g@2x.png">
-          </div>
-        </div>
-      </div>
-      
-      <!-- 商品数据 -->
-      <div class="good good_report">
-        <div class="title">
-          <div class="left-block"></div>
-          <div class="text">商品数据</div>
-          <div class="report-icon">
-            <img src="@/assets/images/shop_report@2x.png">
-          </div>
-          <div class="next-btn">
-            <img src="@/assets/images/arrow_enter_b@2x.png">
-          </div>
-        </div>
-        <div class="good__bd">
-          <!--<div class="shop-data">-->
+          <div class="good__bd">
+            <!--<div class="shop-data">-->
             <!--<div>今日销量 {{shopData.sales}}</div>-->
             <!--<div>剩余库存 {{shopData.inventory}}</div>-->
             <!--<div>动销率 {{shopData.mprate}}</div>-->
             <!--<div>库销比 {{shopData.sinkToPin}}</div>-->
-          <!--</div>-->
-          <div class="good-data">
-            <div>动销率 {{goodData.mprate}}</div>
-            <div>库销比 {{goodData.sinkToPin}}</div>
-            <div>退货率 {{goodData.returnRate}}</div>
-          </div>
-        </div>
-      </div>
-      
-      <!-- 商品评价 -->
-      <div class="good good_comment">
-        <div class="title">
-          <div class="left-block"></div>
-          <div class="text">商品评价</div>
-          <div class="all-text">查看全部</div>
-          <div class="next-btn">
-            <img src="@/assets/images/arrow_enter_b@2x.png">
-          </div>
-        </div>
-        <div class="good__bd">
-          <div class="keyword-box">
-            <div class="keyword"
-                 v-for="(keyword, keywordIndex) in commentKeywords"
-                 :key="keywordIndex">{{keyword}}</div>
-          </div>
-          <div class="comment-box"
-               v-for="(comment, commentIndex) in comments"
-               :key="commentIndex">
-            <div class="author">
-              <div class="portrait">
-                <!--<img :src="comment.author.portrait">-->
-              </div>
-              <div class="nickname">{{comment.author.name}}</div>
-              <div class="like">
-                <div class="icon">
-                  <img v-if="comment.isLike"
-                       src="@/assets/images/fabulous_pre_b@2x.png">
-                  <img  v-else
-                        src="@/assets/images/fabulous_nor@2x.png">
-                </div>
-                <div class="num" v-if="comment.like">{{comment.like}}</div>
-              </div>
-            </div>
-            <div class="text">{{comment.text}}</div>
-            <div class="images">
-              <div class="image"
-                   v-for="(image, imageIndex) in comment.images"
-                   :key="imageIndex">
-                <!--<img :src="image.url">-->
-              </div>
+            <!--</div>-->
+            <div class="good-data">
+              <div>动销率 {{goodData.mprate}}</div>
+              <div>库销比 {{goodData.sinkToPin}}</div>
+              <div>退货率 {{goodData.returnRate}}</div>
             </div>
           </div>
         </div>
-      </div>
-      
-      <!-- 相似商品-->
-      <div class="good good_similar">
-        <div class="title">
-          <div class="left-block"></div>
-          <div class="text">相似商品</div>
-        </div>
-        <div class="good__bd">
-          <div class="similar-box"
-               v-for="(similar, similarIndex) in similars"
-               :key="similarIndex">
-            <div class="img">
-              <!--<img :src="similar.headId">-->
-            </div>
-            <div class="name">{{similar.name}}</div>
-            <div class="price">
-              <div class="icon">
-                <img src="@/assets/images/shop_money@2x.png">
-              </div>
-              <div class="num">{{similar.price}}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <!-- 商品详情 -->
-      <div class="good good_detail">
-        <div class="title">
-          <div class="left-block"></div>
-          <div class="text">商品详情</div>
-        </div>
-        <div class="good__bd">
-          <div class="image">
-            <img src="@/assets/images/img1@2x.png">
-          </div>
-          <div class="image">
-            <img src="@/assets/images/img2@2x.png">
-          </div>
-          <div class="image">
-            <img src="@/assets/images/img3@2x.png">
-          </div>
-        </div>
-      </div>
-    </div>
     
-    <!-- 底部功能栏 -->
-    <div class="footer">
-      <div class="help-btn">
-        <div class="icon">
-          <img src="@/assets/images/shop_service@2x.png"/>
+        <!-- 商品评价 -->
+        <div class="good good_comment">
+          <div class="title">
+            <div class="left-block"></div>
+            <div class="text">商品评价</div>
+            <div class="all-text">查看全部</div>
+            <div class="next-btn">
+              <img src="@/assets/images/arrow_enter_b@2x.png">
+            </div>
+          </div>
+          <div class="good__bd">
+            <div class="keyword-box">
+              <div class="keyword"
+                   v-for="(keyword, keywordIndex) in commentKeywords"
+                   :key="keywordIndex">{{keyword}}</div>
+            </div>
+            <div class="comment-box"
+                 v-for="(comment, commentIndex) in comments"
+                 :key="commentIndex">
+              <div class="author">
+                <div class="portrait">
+                  <!--<img :src="comment.author.portrait">-->
+                </div>
+                <div class="nickname">{{comment.author.name}}</div>
+                <div class="like">
+                  <div class="icon">
+                    <img v-if="comment.isLike"
+                         src="@/assets/images/fabulous_pre_b@2x.png">
+                    <img  v-else
+                          src="@/assets/images/fabulous_nor@2x.png">
+                  </div>
+                  <div class="num" v-if="comment.like">{{comment.like}}</div>
+                </div>
+              </div>
+              <div class="text">{{comment.text}}</div>
+              <div class="images">
+                <div class="image"
+                     v-for="(image, imageIndex) in comment.images"
+                     :key="imageIndex">
+                  <!--<img :src="image.url">-->
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="text">客服</div>
-      </div>
-      <div class="collect-btn">
-        <div class="icon">
-          <img src="@/assets/images/shop_collect@2x.png"/>
+    
+        <!-- 相似商品-->
+        <div class="good good_similar">
+          <div class="title">
+            <div class="left-block"></div>
+            <div class="text">相似商品</div>
+          </div>
+          <div class="good__bd">
+            <div class="similar-box"
+                 v-for="(similar, similarIndex) in similars"
+                 :key="similarIndex">
+              <div class="img">
+                <!--<img :src="similar.headId">-->
+              </div>
+              <div class="name">{{similar.name}}</div>
+              <div class="price">
+                <div class="icon">
+                  <img src="@/assets/images/shop_money@2x.png">
+                </div>
+                <div class="num">{{similar.price}}</div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="text">收藏</div>
+    
+        <!-- 商品详情 -->
+        <div class="good good_detail">
+          <div class="title">
+            <div class="left-block"></div>
+            <div class="text">商品详情</div>
+          </div>
+          <div class="good__bd">
+            <div class="image">
+              <img src="@/assets/images/img1@2x.png">
+            </div>
+            <div class="image">
+              <img src="@/assets/images/img2@2x.png">
+            </div>
+            <div class="image">
+              <img src="@/assets/images/img3@2x.png">
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="add-list-btn">加入进货单</div>
-      <div class="stock-btn">立即进货</div>
-    </div>
+  
+      <!-- 底部功能栏 -->
+      <div class="footer">
+        <div class="help-btn">
+          <div class="icon">
+            <img src="@/assets/images/shop_service@2x.png"/>
+          </div>
+          <div class="text">客服</div>
+        </div>
+        <div class="collect-btn">
+          <div class="icon">
+            <img src="@/assets/images/shop_collect@2x.png"/>
+          </div>
+          <div class="text">收藏</div>
+        </div>
+        <div class="add-list-btn">加入进货单</div>
+        <div class="stock-btn">立即进货</div>
+      </div>
   </div>
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
   export default {
     name: "goodDetail",
     data() {
@@ -280,9 +281,18 @@
           price: 39.6
         }],
         // imageList: [],
+        scroll: null,
       }
     },
     created() {
+    },
+    mounted() {
+      // if (this.$refs.wrapper) {
+      //   this.scroll = new Bscroll(this.$refs.wrapper, {
+      //     click: true,
+      //     bounce: false
+      //   });
+      // }
     },
     methods: {}
   }
