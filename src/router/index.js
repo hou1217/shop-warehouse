cb16d4cbd9c991611b837f4550b325cf63d19a23
houyishuai <hou1217@foxmail.com>
2019-05-06 14:00:49 +0800

    2019年5月6日13:59:59 1.改变webpack打包的js的chunkName

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ export default new Router({
     {
       path: '/home',
       name: 'home',
-      component: () => import('@/views/home/Home.vue'),
+      component: () => import(/* webpackChunkName: "home" */ '@/views/home/Home.vue'),
       redirect:'/home/category/3',
       children:[
         //分类商品
@@ -22,24 +22,24 @@ export default new Router({
           // route level code-splitting
           // this generates a separate chunk (about.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
-          component: () => import('@/views/category/GoodsCategory.vue')
+          component: () => import(/* webpackChunkName: "category" */ '@/views/category/GoodsCategory.vue')
         }
       ]
     },
     {
       path: '/stock/:type',
       name: 'stock',
-      component: () => import('@/views/stock/ShopStock.vue'),
+      component: () => import(/* webpackChunkName: "stock" */ '@/views/stock/ShopStock.vue'),
     },
     {
       path: '/goodsGeneralStock/:type',
       name: 'general',
-      component: () => import('@/views/goodsGeneralStock/GoodsGeneralStock.vue'),
+      component: () => import(/* webpackChunkName: "general" */ '@/views/goodsGeneralStock/GoodsGeneralStock.vue'),
     },
     {
       path: '/goodsDetail',
       name: 'goodsDetail',
-      component: () => import('@/views/goodsDetail/GoodsDetail')
+      component: () => import(/* webpackChunkName: "goodsDetail" */ '@/views/goodsDetail/GoodsDetail')
     },
    
     // 店铺报表
@@ -47,109 +47,109 @@ export default new Router({
       path: '/shopReport/:type',
       name: 'shopReport',
       meta:{ notScrollToTop: true },
-      component: () => import('@/views/shopReport/ShopReport')
+      component: () => import(/* webpackChunkName: "shopReport" */ '@/views/shopReport/ShopReport')
     },
     // 店铺报表-历史记录
     {
       path: '/historyRecord',
       name: 'historyRecord',
-      component: () => import('@/views/shopReport/HistoryRecord')
+      component: () => import(/* webpackChunkName: "historyRecord" */ '@/views/shopReport/HistoryRecord')
     },
     // 店铺报表-店铺数据
     {
       path: '/shopData',
       name: 'shopData',
-      component: () => import('@/views/shopReport/ShopData')
+      component: () => import(/* webpackChunkName: "shopData" */ '@/views/shopReport/ShopData')
     },
     // 店铺报表-分类数据
     {
       path: '/classifiedData',
       name: 'classifiedData',
-      component: () => import('@/views/shopReport/ClassifiedData')
+      component: () => import(/* webpackChunkName: "classifiedData" */ '@/views/shopReport/ClassifiedData')
     },
     // 店铺报表-单类数据
     {
       path: '/singleClassData',
       name: 'singleClassData',
-      component: () => import('@/views/shopReport/SingleClassData')
+      component: () => import(/* webpackChunkName: "singleClassData" */ '@/views/shopReport/SingleClassData')
     },
     // 店铺报表-已进货商品数据分析
      {
       path: '/stockedGoodsReport',
       name: 'stockedGoodsReport',
-      component: () => import('@/views/shopReport/StockedGoodsReport')
+      component: () => import(/* webpackChunkName: "stockedGoodsReport" */ '@/views/shopReport/StockedGoodsReport')
     },
     // 未进货数据分析
     {
       path: '/noStockedGoodsReport',
       name: 'noStockedGoodsReport',
-      component: () => import('@/views/noStockedGoodsReport/NoStockedGoodsReport')
+      component: () => import(/* webpackChunkName: "noStockedGoodsReport" */ '@/views/noStockedGoodsReport/NoStockedGoodsReport')
     },
     //选择对比商品
     {
       path: '/compareSelect',
       name: 'compareSelect',
-      component: () => import('@/views/compareSelect/CompareSelect')
+      component: () => import(/* webpackChunkName: "compareSelect" */ '@/views/compareSelect/CompareSelect')
     },
     // 总仓报表
     {
       path: '/warehouseReport',
       name: 'warehouseReport',
-      component: () => import('@/views/warehouseReport/WarehouseReport')
+      component: () => import(/* webpackChunkName: "warehouseReport" */ '@/views/warehouseReport/WarehouseReport')
     },
     // 进货单-列表
     {
       path: '/purchaseOrder',
       name: 'purchaseOrder',
-      component: () => import('@/views/purchaseOrder/PurchaseOrder')
+      component: () => import(/* webpackChunkName: "purchaseOrder" */ '@/views/purchaseOrder/PurchaseOrder')
     },
       // 进货单-进货单管理
     {
       path: '/purchaseOrderManage',
       name: 'purchaseOrderManage',
-      component: () => import('@/views/purchaseOrder/PurchaseOrderManage')
+      component: () => import(/* webpackChunkName: `purchaseOrderManage` */ '@/views/purchaseOrder/PurchaseOrderManage')
     },
       // 进货单-进货确认
     {
       path: '/purchaseOrderConfirm',
       name: 'purchaseOrderConfirm',
-      component: () => import('@/views/purchaseOrder/PurchaseOrderConfirm')
+      component: () => import(/* webpackChunkName: `purchaseOrderConfirm` */ '@/views/purchaseOrder/PurchaseOrderConfirm')
     },
       // 进货单-进货订单详情
     {
       path: '/purchaseOrderDetail',
       name: 'purchaseOrderDetail',
-      component: () => import('@/views/purchaseOrder/PurchaseOrderDetail')
+      component: () => import(/* webpackChunkName: `purchaseOrderDetail` */ '@/views/purchaseOrder/PurchaseOrderDetail')
     },
       // 进货单-进货清单列表
     {
       path: '/purchaseOrderList',
       name: 'purchaseOrderList',
-      component: () => import('@/views/purchaseOrder/PurchaseOrderList')
+      component: () => import(/* webpackChunkName: `purchaseOrderList` */ '@/views/purchaseOrder/PurchaseOrderList')
     },
       // 进货单-下单状态
     {
       path: '/purchaseOrderStatus',
       name: 'purchaseOrderStatus',
-      component: () => import('@/views/purchaseOrder/PurchaseOrderStatus')
+      component: () => import(/* webpackChunkName: `purchaseOrderStatus` */ '@/views/purchaseOrder/PurchaseOrderStatus')
     },
     // 店铺订单
     {
       path: '/shopOrder',
       name: 'shopOrder',
-      component: () => import('@/views/shopOrder/ShopOrder')
+      component: () => import(/* webpackChunkName: `shopOrder` */ '@/views/shopOrder/ShopOrder')
     },
       // 主管-报表
     {
       path: '/generalManagerReport',
       name: 'generalManagerReport',
-      component: () => import('@/views/generalManagerReport/GeneralManagerReport')
+      component: () => import(/* webpackChunkName: `generalManagerReport` */ '@/views/generalManagerReport/GeneralManagerReport')
     },
       // 主管-地图
     {
       path: '/map',
       name: 'map',
-      component: () => import('@/views/map/index')
+      component: () => import(/* webpackChunkName: `map` */ '@/views/map/index')
     }
   ],
   scrollBehavior(to, from, savedPosition) {
